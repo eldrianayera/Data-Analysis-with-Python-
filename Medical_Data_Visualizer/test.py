6895513fd554e34cf18025812921404afccdf200
Eldrian <eldrianayera710@gmail.com>
2025-07-16 16:28:12 +0800

    Medical_data_visualizer : add df_car by pd.melt

--- Medical_Data_Visualizer/test.py
+++ Medical_Data_Visualizer/test.py
@@ -9,18 +9,24 @@ df = pd.read_csv('medical_examination.csv')
 # 2
 df['overweight'] =  (df['weight'] / ((df['height']/100) ** 2) > 25).astype(int)
 
-print(df)
-print(df['overweight'])
+
 
 
 # 3
-df['cholesterol'] = df.loc
+df['cholesterol'] = (df['cholesterol'] > 1).astype(int) 
+df['gluc'] = (df['gluc'] > 1).astype(int) 
+
+print(df)
+
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(
+        df,
+        id_vars = ['cardio'],
+        value_vars = ['cholestrol' , 'gluc' , 'smoke']
+    )
 
     # 6
     df_cat = None
